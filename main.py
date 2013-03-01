128e94f34c362d56acd5ef2888e2a365f96397b4
turbolaser <turbolaser@example.com>
2013-02-28 19:04:31 -0500

    Switch to using the pi-blaster PWM software. Add oscillation support.

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import flask
-import RPi.GPIO as gpio
-import time
-import threading
+import gevent
+from gevent import monkey; monkey.patch_all()
 
 # Duration of the trigger pull in seconds
-TRIGGER_TIME = 2
-# The GPIO to use for pulling the trigger
-TRIGGER_PIN = 11
+TRIGGER_TIME = 0.5
+# The PWM channel to use for pulling the trigger
+TRIGGER_PIN = 7 # 7= P1 pin 22
+
+# Left oscillation pulse width (in hundredths of seconds - 0.1 for 1 ms)
+OSC_LEFT_WIDTH = 0.1
+# Right oscillation pulse width
+OSC_RIGHT_WIDTH = 0.2
+# Oscillation time (1 complete cycle, in seconds)
+OSC_PERIOD = 0.2
+# Oscillation channel
+OSC_CHANNEL = 0 # 0= P1 pin 7
+
+osc_thread = None
+osc_end = False
 
 nerfer = flask.Flask(__name__)
 
@@ -14,19 +25,56 @@ nerfer = flask.Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@nerfer.route('/fire')
+@nerfer.route('/fire', methods=['GET', 'POST'])
 def fire_dart():
-    gpio.output(TRIGGER_PIN, gpio.HIGH)
-    end_thread = threading.Thread(target=cancel_fire)
-    end_thread.start()
+    fire_thread = gevent.spawn(open_fire)
     return 'pew!'
 
-def cancel_fire():
-    time.sleep(TRIGGER_TIME)
-    gpio.output(TRIGGER_PIN, gpio.LOW)
+def open_fire():
+    pwm_file = open('/dev/pi-blaster', 'w', 1)
+    pwm_file.write('%s=1\n' % TRIGGER_PIN) 
+    gevent.sleep(TRIGGER_TIME)
+    pwm_file.write('%s=0\n' % TRIGGER_PIN)
+
+@nerfer.route('/osc', methods=['GET', 'POST'])
+def begin_oscillation():
+    global osc_thread
+    global osc_end
+
+    stop_oscillation()
+
+    if osc_thread is None:
+        osc_thread = gevent.spawn(osc_loop)    
+
+    return 'oscillating'
+
+@nerfer.route('/stop-osc', methods=['GET', 'POST'])
+def stop_oscillation():
+    global osc_thread
+    global osc_end
+
+    if osc_thread is None:
+        return 'done oscillating'
+
+    if not osc_thread.ready():
+        osc_end = True
+
+    osc_thread = None
+    return 'done oscillating'
+
+def osc_loop():
+    global osc_end
+    pwm_file = open('/dev/pi-blaster', 'w', 1)
+    while not osc_end:
+        pwm_file.write('%s=%s\n' % (OSC_CHANNEL, OSC_LEFT_WIDTH))
+        gevent.sleep(OSC_PERIOD / 2.0)
+        pwm_file.write('%s=%s\n' % (OSC_CHANNEL, OSC_RIGHT_WIDTH))
+        gevent.sleep(OSC_PERIOD / 2.0) 
+
+    pwm_file.write('%s=0\n' % OSC_CHANNEL)
+    osc_end = False
+
 
 if __name__ == '__main__':
-    gpio.setmode(gpio.BCM)
-    gpio.setup(TRIGGER_PIN, gpio.OUT)
+    nerfer.debug = True
     nerfer.run(host='0.0.0.0')
-    gpio.cleanup()

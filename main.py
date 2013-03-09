03d1577f11537ab792ac95c8bf9747af0b8af8fd
turbolaser <turbolaser@example.com>
2013-03-09 03:24:33 -0500

    Some tweaks to servo speed based on feedback from Upverter team. Add basic web controls.

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import gevent
 from gevent import monkey; monkey.patch_all()
 
 # Duration of the trigger pull in seconds
-TRIGGER_TIME = 0.5
+TRIGGER_TIME = 0.28
 # The PWM channel to use for pulling the trigger
 TRIGGER_PIN = 7 # 7= P1 pin 22
 
 # Left oscillation pulse width (in hundredths of seconds - 0.1 for 1 ms)
-OSC_LEFT_WIDTH = 0.1
+OSC_LEFT_WIDTH = 0.8
 # Right oscillation pulse width
-OSC_RIGHT_WIDTH = 0.2
-# Oscillation time (1 complete cycle, in seconds)
-OSC_PERIOD = 0.2
+OSC_RIGHT_WIDTH = 0.9
+# Oscillation time (1/2 cycle, in seconds)
+OSC_PERIOD = 1.0
+# Oscillation paruse time, in seconds
+OSC_PAUSE = 0.5
 # Oscillation channel
 OSC_CHANNEL = 0 # 0= P1 pin 7
 
@@ -30,8 +32,11 @@ TILT_CHANNEL = 1 # 1= P1 pin 11
 
 nerfer = flask.Flask(__name__)
 
-@nerfer.route('/')
-def hello_world():
+@nerfer.route('/', methods=['GET', 'POST'])
+def show_controls():
+    if flask.request.method == 'GET':
+        return flask.render_template('control.html')
+
     commands = []
     commands.append('Commands\n')
     commands.append('/fire: Fire a dart\n')
@@ -44,6 +49,8 @@ def hello_world():
 @nerfer.route('/fire', methods=['GET', 'POST'])
 def fire_dart():
     fire_thread = gevent.spawn(open_fire)
+    if flask.request.method == 'GET':
+        return flask.redirect('/')
     return 'pew!\n'
 
 def open_fire():
@@ -62,6 +69,8 @@ def begin_oscillation():
     if osc_thread is None:
         osc_thread = gevent.spawn(osc_loop)    
 
+    if flask.request.method == 'GET':
+        return flask.redirect('/')
     return 'oscillating\n'
 
 @nerfer.route('/osc-stop', methods=['GET', 'POST'])
@@ -70,12 +79,16 @@ def stop_oscillation():
     global osc_end
 
     if osc_thread is None:
+        if flask.request.method == 'GET':
+            return flask.redirect('/')
         return 'done oscillating\n'
 
     if not osc_thread.ready():
         osc_end = True
 
     osc_thread = None
+    if flask.request.method == 'GET':
+        return flask.redirect('/')
     return 'done oscillating\n'
 
 def osc_loop():
@@ -83,9 +96,13 @@ def osc_loop():
     pwm_file = open('/dev/pi-blaster', 'w', 1)
     while not osc_end:
         pwm_file.write('%s=%s\n' % (OSC_CHANNEL, OSC_LEFT_WIDTH))
-        gevent.sleep(OSC_PERIOD / 2.0)
+        gevent.sleep(OSC_PERIOD)
+        pwm_file.write('%s=0\n' % OSC_CHANNEL)
+        gevent.sleep(OSC_PAUSE)
         pwm_file.write('%s=%s\n' % (OSC_CHANNEL, OSC_RIGHT_WIDTH))
-        gevent.sleep(OSC_PERIOD / 2.0) 
+        gevent.sleep(OSC_PERIOD)
+        pwm_file.write('%s=0\n' % OSC_CHANNEL)
+        gevent.sleep(OSC_PAUSE) 
 
     pwm_file.write('%s=0\n' % OSC_CHANNEL)
     osc_end = False
@@ -93,11 +110,15 @@ def osc_loop():
 @nerfer.route('/tilt-up', methods=['GET', 'POST'])
 def begin_tilt_up():
     tilt_thread = gevent.spawn(do_tilt, TILT_UP_WIDTH)
+    if flask.request.method == 'GET':
+        return flask.redirect('/')
     return 'tilt up\n'
 
 @nerfer.route('/tilt-down', methods=['GET', 'POST'])
 def begin_tilt_down():
     tilt_thread = gevent.spawn(do_tilt, TILT_DOWN_WIDTH)
+    if flask.request.method == 'GET':
+        return flask.redirect('/')
     return 'tilt down\n'
 
 def do_tilt(pulse_width):
@@ -107,5 +128,5 @@ def do_tilt(pulse_width):
     pwm_file.write('%s=0\n' % TILT_CHANNEL)
 
 if __name__ == '__main__':
-    # nerfer.debug = True
+    nerfer.debug = True
     nerfer.run(host='0.0.0.0')
